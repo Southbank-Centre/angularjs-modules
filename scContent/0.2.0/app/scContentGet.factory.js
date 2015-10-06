e9b7693f594c61bba1ee5ba1e869ad51a62f141d
samueljmurray <samueljmurray@gmail.com>
2015-10-06 15:27:59 +0100

    remove scContentBaseURL constant

---
+++ scContent/0.2.0/app/scContentGet.factory.js
@@ -0,0 +1,69 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc factory
+   * @name scContentGet
+   * @description
+   *
+   * Provides methods for getting content from the Southbank Centre web content service
+   */
+  angular
+    .module('scContent')
+    .factory('scContentGet', scContentGet);
+
+  scContentGet.$inject = ['$http', '$rootScope', 'scContentBaseURL'];
+
+  function scContentGet($http, $rootScope, scContentBaseURL) {
+
+    var service = {
+      item: item,
+      items: items,
+      itemByPath: itemByPath,
+      itemByURL: itemByURL
+    };
+    return service;
+
+    function item(type, id, params) {
+
+      return $http.get(scContentBaseURL + '/api/' + type + '/' + id + '.json', {params: params})
+        .then(_getComplete)
+        .catch(_getFailed);
+
+    }
+
+    function items(type, params) {
+
+      return $http.get(scContentBaseURL + '/api/' + type + '.json', {params: params})
+        .then(_getComplete)
+        .catch(_getFailed);
+
+    }
+
+    function itemByPath(path, params) {
+
+      return $http.get(scContentBaseURL + '/' + path, {params: params})
+        .then(_getComplete)
+        .catch(_getFailed);
+
+    }
+
+    function itemByURL(URL, params) {
+
+      return $http.get(URL, {params: params})
+        .then(_getComplete)
+        .catch(_getFailed);
+
+    }
+
+    function _getComplete(response) {
+      return response.data;
+    }
+
+    function _getFailed(error) {
+      $rootScope.$broadcast('data-error', error);
+    }
+
+  }
+
+})();
